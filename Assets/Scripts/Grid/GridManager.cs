1c4e37147d753b468e4f26a09e05c49712e996d7
Nikita Anufriev <nikita9970@gmail.com>
2024-04-20 19:33:58 +0300

    Added logic for swap handling and board refill

--- Assets/Scripts/Grid/GridManager.cs
+++ Assets/Scripts/Grid/GridManager.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
+using System.Linq;
 using Cysharp.Threading.Tasks;
 using Match3BaseMechanic.Helpers;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Match3BaseMechanic.Grid
 {
@@ -14,14 +17,17 @@ namespace Match3BaseMechanic.Grid
         private readonly GameObject _baseGridElement;
         private readonly Transform _baseGridParent;
         private readonly Camera _mainCamera;
-        
+
         private TileElementMono[,] _tiles;
         private TileElementMono _selectedTile;
+        private List<TileElementMono> _matchedTiles;
+        private HashSet<int> _columnsToUpdate;
+        private bool _isBoardProcessing;
 
         public GridManager(
-            GameConfig config, 
-            TileElementMono tileElementMonoPrefab, 
-            Transform tilePoolParent, 
+            GameConfig config,
+            TileElementMono tileElementMonoPrefab,
+            Transform tilePoolParent,
             Transform tileGridParent,
             Transform baseGridParent,
             GameObject baseGridElement,
@@ -35,6 +41,8 @@ namespace Match3BaseMechanic.Grid
             _baseGridElement = baseGridElement;
             _mainCamera = mainCamera;
             _tileElementPool = new TileElementPoolManager();
+            _matchedTiles = new List<TileElementMono>();
+            _columnsToUpdate = new HashSet<int>();
         }
 
         public async UniTask Init()
@@ -53,66 +61,216 @@ namespace Match3BaseMechanic.Grid
                 newTile.transform.position = new Vector2(x, y);
             }
         }
-        
+
         private async UniTask CreateGrid()
         {
             _tiles = new TileElementMono[_config.GridWidth, _config.GridHeight];
             for (var x = 0; x < _config.GridWidth; x++)
             for (var y = 0; y < _config.GridHeight; y++)
             {
-                var newTile = await _tileElementPool.GetElementFromPool();
-                newTile.transform.position = new Vector2(x, y);
-                newTile.Init(x, y, GetRandomColor(), _mainCamera);
-                newTile.OnTileClick += SelectTile;
-                newTile.OnTileSwap += SwipeTile;
-                newTile.transform.parent = _tileGridParent;
-                newTile.gameObject.SetActive(true);
-                _tiles[x, y] = newTile;
+                await SpawnTile(x, y);
             }
         }
 
-        public void SelectTile(TileElementMono tile)
+        private void SelectTile(TileElementMono tile)
         {
+            if (_isBoardProcessing)
+                return;
+
             if (_selectedTile == null)
-            {
                 _selectedTile = tile;
-            }
             else
-            {
-                SwapTiles(_selectedTile, tile);
-                _selectedTile = null;
-            }
+                SwapTiles(_selectedTile, tile).ContinueWith(() => _selectedTile = null).Forget();
         }
 
         private void SwipeTile(TileElementMono tile, Vector3 direction)
         {
+            if (_isBoardProcessing)
+                return;
+
             var targetX = tile.PositionX + (int)direction.x;
             var targetY = tile.PositionY + (int)direction.y;
             if (targetX >= 0 && targetX < _config.GridWidth && targetY >= 0 && targetY < _config.GridHeight)
             {
                 var targetTile = _tiles[targetX, targetY];
                 if (targetTile != null)
-                    SwapTiles(tile, targetTile);
+                    SwapTiles(tile, targetTile).Forget();
             }
         }
-        
-        private void SwapTiles(TileElementMono tile1, TileElementMono tile2)
-        {
-            if (Mathf.Abs(tile1.PositionX - tile2.PositionX) + Mathf.Abs(tile1.PositionY - tile2.PositionY) == 1)
-            {
-                (tile1.transform.position, tile2.transform.position) = (tile2.transform.position, tile1.transform.position);
 
-                var tempX = tile1.PositionX;
-                var tempY = tile1.PositionY;
-                tile1.SetNewPositionIndex(tile2.PositionX, tile2.PositionY);
-                tile2.SetNewPositionIndex(tempX, tempY);
-                
-                //CheckMatches
-            }
-            else
+        private async UniTask SwapTiles(TileElementMono tile1, TileElementMono tile2)
+        {
+            try
             {
-                //Can't swap
+                _isBoardProcessing = true;
+
+                if (Mathf.Abs(tile1.PositionX - tile2.PositionX) + Mathf.Abs(tile1.PositionY - tile2.PositionY) == 1)
+                {
+                    SwapTilePositions(tile1, tile2);
+
+                    if (CheckMatches(tile1) || CheckMatches(tile2))
+                    {
+                        _matchedTiles = _matchedTiles.Distinct().ToList();
+                        HandleMatches();
+                        await RefillBoard();
+                        _matchedTiles.Clear();
+                        _columnsToUpdate.Clear();
+                    }
+                    else
+                    {
+                        SwapTilePositions(tile1, tile2);
+                    }
+                }
+                else
+                {
+                    _selectedTile = null;
+                }
             }
+            finally
+            {
+                _isBoardProcessing = false;
+            } 
+        }
+
+        private void HandleMatches()
+        {
+            foreach (var matchedTile in _matchedTiles)
+            {
+                RemoveTile(matchedTile);
+                _tiles[matchedTile.PositionX, matchedTile.PositionY] = null;
+            }
+        }
+
+        private async UniTask RefillBoard()
+        {
+            TilesFall();
+            await SpawnNewTiles();
+        }
+
+        private void TilesFall()
+        {
+            foreach (var tile in _matchedTiles)
+            {
+                _columnsToUpdate.Add(tile.PositionX);
+            }
+
+            foreach (var x in _columnsToUpdate)
+            {
+                for (var y = 0; y < _config.GridHeight; y++)
+                {
+                    if (_tiles[x, y] == null)
+                    {
+                        for (var j = y + 1; j < _config.GridHeight; j++)
+                        {
+                            if (_tiles[x, j] != null)
+                            {
+                                _tiles[x, j].transform.position = new Vector2(x, y);
+                                _tiles[x, y] = _tiles[x, j];
+                                _tiles[x, j] = null;
+                                _tiles[x, y].SetNewPositionIndex(x, y);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private async UniTask SpawnNewTiles()
+        {
+            var spawningTasks = new List<UniTask>();
+
+            foreach (var x in _columnsToUpdate)
+            {
+                var emptyCount = 0;
+                for (var y = 0; y < _config.GridHeight; y++)
+                {
+                    if (_tiles[x, y] == null)
+                    {
+                        emptyCount++;
+                    }
+                }
+
+                for (var i = 0; i < emptyCount; i++)
+                {
+                    var spawnY = _config.GridHeight - emptyCount + i;
+                    spawningTasks.Add(SpawnTile(x, spawnY));
+                }
+            }
+
+            await UniTask.WhenAll(spawningTasks);
+        }
+
+        private void SwapTilePositions(TileElementMono tile1, TileElementMono tile2)
+        {
+            (tile1.transform.position, tile2.transform.position) = (tile2.transform.position, tile1.transform.position);
+
+            _tiles[tile1.PositionX, tile1.PositionY] = tile2;
+            _tiles[tile2.PositionX, tile2.PositionY] = tile1;
+
+            var tempX = tile1.PositionX;
+            var tempY = tile1.PositionY;
+            tile1.SetNewPositionIndex(tile2.PositionX, tile2.PositionY);
+            tile2.SetNewPositionIndex(tempX, tempY);
+        }
+
+        private bool CheckMatches(TileElementMono tile)
+        {
+            CheckLineMatch(tile, Vector2.right);
+            CheckLineMatch(tile, Vector2.left);
+            CheckLineMatch(tile, Vector2.up);
+            CheckLineMatch(tile, Vector2.down);
+
+            return _matchedTiles.Count > 0;
+        }
+
+
+        private void CheckLineMatch(TileElementMono startTile, Vector2 direction)
+        {
+            var lineMatches = new List<TileElementMono> { startTile };
+            var consecutiveCount = 1;
+
+            var currentTile = startTile;
+            var nextX = startTile.PositionX + (int)direction.x;
+            var nextY = startTile.PositionY + (int)direction.y;
+
+            while (nextX >= 0 && nextX < _config.GridWidth && nextY >= 0 && nextY < _config.GridHeight)
+            {
+                var nextTile = _tiles[nextX, nextY];
+                if (nextTile != null && nextTile.Color == currentTile.Color)
+                {
+                    lineMatches.Add(nextTile);
+                    consecutiveCount++;
+                    nextX += (int)direction.x;
+                    nextY += (int)direction.y;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (consecutiveCount >= 3)
+                _matchedTiles.AddRange(lineMatches);
+        }
+
+        private async UniTask SpawnTile(int x, int y)
+        {
+            var newTile = await _tileElementPool.GetElementFromPool();
+            newTile.transform.position = new Vector2(x, y);
+            newTile.Init(x, y, GetRandomColor(), _mainCamera);
+            newTile.OnTileClick += SelectTile;
+            newTile.OnTileSwap += SwipeTile;
+            newTile.transform.parent = _tileGridParent;
+            newTile.gameObject.SetActive(true);
+            _tiles[x, y] = newTile;
+        }
+
+        private void RemoveTile(TileElementMono tile)
+        {
+            tile.OnTileClick -= SelectTile;
+            tile.OnTileSwap -= SwipeTile;
+            _tileElementPool.ReturnToPool(tile);
         }
 
         private Color GetRandomColor()
